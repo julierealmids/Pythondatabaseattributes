c1631ba6cf21b1122e4a11af26da4d5b3ab00f15
julierealmids <jullietnakayizaa@gmail.com>
2022-09-07 20:23:43 +0300

    Wallet

--- wallet/forms.py
+++ wallet/forms.py
@@ -1,7 +1,8 @@
 from dataclasses import fields
 from pyexpat import model
+from sqlite3 import paramstyle
 from django import forms
-from .models import Currency, Customer, Wallet
+from .models import Account, Card, Currency, Customer, Loan_model, Notification, Receipt, Reward, Third_Party, Transaction, Wallet
 
 class CustomerRegistrationForm(forms.ModelForm):
     class Meta:
@@ -16,4 +17,38 @@ class RegisterCurrency(forms.ModelForm):
 class RegisterWallet(forms.ModelForm):
     class Meta:
         model=Wallet
+        fields="__all__"
+        
+class RegisterAccount(forms.ModelForm):
+    class Meta:
+        model=Account
+        fields="__all__"
+        
+class RegisterThird_party(forms.ModelForm):
+    class Meta:
+        model=Third_Party
+        fields="__all__"
+class RegisterTransaction(forms.ModelForm):
+    class Meta:
+        model=Transaction
+        fields="__all__"
+class RegisterCard(forms.ModelsForm):
+    class Meta:
+        model=Card
+        feilds="__all__"
+class RegisterReceipt(forms.ModelForms):
+    class Meta:
+        model=Receipt
+        fields="__all__"
+class RegisterNotification(forms.ModelForms):
+    class Meta:
+        model=Notification
+        fields="__all__"
+class RegisterLoan_model(forms.ModelForms):
+    class Meta:
+        model=Loan_model
+        fields="__all__"
+class RegisterReward(forms.ModelForm):
+    class Meta:
+        model=Reward
         fields="__all__"
